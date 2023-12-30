a25f22efd8970da5fa13f24d9c8f28501658663c
Vinicius Prado <developer.vinicius.prado@gmail.com>
2023-12-30 11:08:22 -0300

    feat(typescript): starting with typescript
    - implements a boolean variable isValid and checks if the variable age is greatter than 30.

--- typescript/datatypes.js
+++ typescript/datatypes.js
@@ -11,3 +11,9 @@ let ageWithoutMounths = parseInt(ageWithMounths);
 console.log('Age in years', age);
 console.log('Age with mounths', ageWithMounths);
 console.log('Age without mounths', ageWithoutMounths);
+// boolean
+let isValid = false;
+if (age > 30) {
+    isValid = true;
+}
+console.log(isValid);
--- typescript/datatypes.ts
+++ typescript/datatypes.ts
@@ -20,4 +20,12 @@ console.log('Age in years', age);
 console.log('Age with mounths', ageWithMounths);
 console.log('Age without mounths', ageWithoutMounths);
 
+// boolean
+
+let isValid: boolean = false;
+
+if(age > 30){
+    isValid = true
+}
+console.log(isValid);
 
